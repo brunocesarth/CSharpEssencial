e7a294b03a1babc4018c2f9b0411b60a594891c6
Bruno Thobias <bruno.cm.thobias@hotmail.com>
2023-06-28 10:03:41 -0300

    Aula -  Precedência e associatividade de operadores

---
+++ CSFundamentos/PrecedenciaOperadores1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("## Precedência de operadores ##\n");
+
+int x = (10 - 2) * 3;
+Console.WriteLine(x);
+
+bool bo = !(9 != 8) && 5 >= 7 || 8 >= 6;
+Console.WriteLine(bo);
+
+bo = !(9 != 8) && (5 >= 7 || 8 >= 6);
+Console.WriteLine(bo);
+
+int a = 5, b = 6, c = 4;
+int r = ((--a) * b) - (++c);
+Console.WriteLine(r);
+
+Console.ReadKey();
