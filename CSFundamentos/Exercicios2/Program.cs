744ea323c8072c0b1f9fa000a6bba54eade19a57
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2023-06-29 21:02:06 -0300

    Update Program.cs

--- CSFundamentos/Exercicios2/Program.cs
+++ CSFundamentos/Exercicios2/Program.cs
@@ -1,5 +1,12 @@
 ﻿Console.WriteLine("Exercícios fase 2");
 
+Console.WriteLine("Digite um número:");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("digite mais um número:");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+string r = x
+
 // 1 - Declare as variáveis nome, idade e nota atribuindo os valores "Paulo", 17 e 7.5 e exiba a saída no formato: Aluno <nome> tem <idade> anos e nota <nota> usando a concatenção e a interpolação de strings.
 string nome = "Paulo";
 int idade = 17;
@@ -80,3 +87,33 @@ Console.WriteLine(valor1 % valor2);
 //8 - Faça um programa para calcular o resultado da fórmula de baskara dados os valores de a b e c .
 int a = 1, b = 12, c = -13;
 
+// 9- Escreva um programa que receba um nome e uma senha via teclado. Nome é uma string e Senha é um inteiro. Se o nome for igual a ‘admin’ ou ‘maria’ e a senha for igual a ‘123’ então exiba a mensagem ‘Login feito com sucesso’ caso contrário exiba a mensagem ‘Login inválido’: (use o operador condicional ternário)
+
+Console.Write("Digite um nome: ");
+nome = Console.ReadLine();
+Console.Write("Digite uma senha: ");
+int senha = Convert.ToInt32(Console.ReadLine());
+
+string r = (nome == "admin" || nome == "maria") && senha == 123 ? "Login feito com sucesso" : "Login inválido";
+
+Console.WriteLine(r);
+
+/*
+ 10- Indique verdadeiro(V) ou falso (F) para as seguintes declarações de variáveis considerando 
+a nomenclatura usada:
+(V) string? nome; é um exemplo de nullable reference type;
+(F) para x igual a 0 , Console.WriteLine(x++); imprime o valor 1
+(V) A ordem de precedência dos operadores lógicos é : ! , || e && (NOT, OR e AND)
+(V) para y igual a 0 , Console.WriteLine(++y); imprime o valor 1
+(F) (10 % 2 == 0) ? “Par” : “Impar”; Vai retornar “Impar”
+(F) Para x=25 e y=5 , a expressão ( y >= x ) && ( y <= x ); retorna true;
+(V) Os nullable reference types emitem um alerta em tempo de compilação para uso do null
+(F) Para x = 10 , o código Console.WriteLine(x+=x); imprime o valor 10;
+(F) Para x= 5 , a expressão !(9 >= x) && x <= 7 || x == 5); retorna false;
+(V) O operador ?. permite verificar se um objeto é null e evitar o NullReferenceException
+(V) Qualquer tipo de dado pode ser declarado como anulável com a ajuda do operador "?"
+
+11- Escreva um programa que recebe via teclado dois números inteiros x e y e imprima no 
+console se x é par ou não e se y é par ou não. Use o operador condicional ternário (? :)
+ */
+int 
