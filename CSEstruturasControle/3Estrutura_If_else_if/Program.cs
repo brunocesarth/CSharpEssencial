e629862d3444c0fb3dfe22f930301917ff96b860
Bruno Thobias <bruno.cm.thobias@hotmail.com>
2023-07-01 00:37:01 -0300

    Aula - Instrucao if-else-if

---
+++ CSEstruturasControle/3Estrutura_If_else_if/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("## Instrução if-else-if ##\n");
+
+Console.WriteLine("\nInforme a nota do aluno \t");
+var nota = Convert.ToDouble(Console.ReadLine());
+
+if (nota < 5)
+{
+    Console.WriteLine("Aluno reprovado");
+}
+else if (nota >= 5 && nota < 6)
+{
+    Console.WriteLine("Aluno em recuperação");
+}
+else if (nota >= 6 && nota <= 9)
+{
+    Console.WriteLine("Aluno Aprovado");
+}
+else
+{
+    Console.WriteLine("Aluno Aprovado com Distinção");
+}
+
+Console.WriteLine("Fim do processamento...");
+Console.ReadKey();
