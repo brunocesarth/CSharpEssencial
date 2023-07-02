e45815f905a1ac3ee08da66aa4edeb7bee2e8ade
Bruno Thobias <bruno.cm.thobias@hotmail.com>
2023-07-02 00:03:51 -0300

    Aula - Estrutura de repetição

---
+++ CSEstruturasControle/5Estrutura_repeticao/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("## Estrutura de repetição com goto/çabeç ##\n");
+
+int i = 1;
+
+repetir:
+
+Console.WriteLine($"i = {i}");
+
+i++;
+
+if (i <= 10)
+    goto repetir;
+
+Console.WriteLine("Fim do processamento...");
+
+Console.ReadKey();
