f498b03a4bf3dab09d4373bf2fd12164116f47bc
Bruno Thobias <bruno.cm.thobias@hotmail.com>
2023-07-02 19:21:22 -0300

    Aula - Instrução do while

---
+++ CSEstruturasControle/7Estrutura_do_while/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("## Instrução do while ##\n");
+
+var i = 1;
+do
+{
+    Console.WriteLine($"i = {i}");
+    i++;
+    if (i > 7)
+        break;
+
+} while (i <= 10);
+
+Console.WriteLine("Fim do processamento...");
+Console.ReadKey();
+
+Console.WriteLine("------------------------");
+
+int x = 0;
+do
+{
+    int y = 0;
+    do
+    {
+        Console.Write($"({x},{y})");
+        y++;
+    } while (y < 5);
+    x++;
+    Console.WriteLine();
+} while (x < 5);
