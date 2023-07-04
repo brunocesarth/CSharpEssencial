573208b524b03ce64404bfd99d08dd8234cb69fa
Bruno Thobias <bruno.cm.thobias@hotmail.com>
2023-07-04 11:16:56 -0300

    Aula - Instruções break e continue

---
+++ CSEstruturasControle/9Break_Continue/Program.cs
@@ -0,0 +1,65 @@
+﻿Console.WriteLine("## break e continue ##\n");
+
+for (; ; )
+{
+    Console.WriteLine("Tecle algo (X sai)");
+    var opcao = Console.ReadLine();
+
+    Console.WriteLine(opcao.ToUpper());
+
+    if (opcao == "X" || opcao == "x")
+    {
+        Console.WriteLine("Tchau...");
+        break;
+    }
+}
+
+Console.WriteLine("\nFim do processamento...");
+Console.ReadKey();
+
+Console.WriteLine("-------------------------");
+
+for (int i = 1; i <= 10; i++)
+{
+    if (i == 4)
+        continue;
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("\nFim do processamento...");
+Console.ReadKey();
+
+
+
+Console.WriteLine("-------------------------");
+
+int n = 1;
+while (n <= 10)
+{
+    if (n == 5)
+    {
+        n = n + 1;
+        continue;
+    }
+    Console.WriteLine($"{n}");
+    n++;
+}
+
+Console.WriteLine("\nFim do processamento...");
+Console.ReadKey();
+
+Console.WriteLine("-------------------------");
+
+for (int i = 0;i <= 10; i++)
+{
+    if (i == 5)
+        continue;
+
+    if (i > 8)
+        break;
+
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("\nFim do processamento...");
+Console.ReadKey();
