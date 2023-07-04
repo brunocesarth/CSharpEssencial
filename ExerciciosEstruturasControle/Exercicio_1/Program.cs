1574644018a5cdcce6338a24ff77d53a6c370fcf
Bruno Thobias <bruno.cm.thobias@hotmail.com>
2023-07-04 11:41:54 -0300

    Exercicio 1 - Feito

---
+++ ExerciciosEstruturasControle/Exercicio_1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("1- Escreva um programa para receber 3 números inteiros e a seguir calcular e exibir qual deles é o maior\r\n");
+Console.WriteLine("Encontre o maior dentre os 3 números: ");
+Console.Write("Primeiro número: \t");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Segundo número: \t");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Segundo número: \t");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 > n2 && n1 > n3)
+{
+    Console.WriteLine($"O primeiro número: {n1} é o maior");
+}
+else if (n2 > n3 && n2 > n1)
+{
+    Console.WriteLine($"O segundo número: {n2} é maior");
+}
+else if (n3 > n1 && n3 > n2)
+{
+    Console.WriteLine($"O terceiro número: {n3} é maior");
+}
+
+Console.ReadKey();
