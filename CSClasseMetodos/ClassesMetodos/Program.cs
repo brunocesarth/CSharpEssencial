24ac3b0ae2d278cc1356b9c849b736775de520c4
Bruno Thobias <bruno.cm.thobias@hotmail.com>
2023-07-12 10:53:01 -0300

    Aula - Classe e Objeto

---
+++ CSClasseMetodos/ClassesMetodos/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("## Classes e Métodos ##\n");
+
+int x = 100;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Maria";
+p1.Idade = 25;
+p1.Sexo = "feminino";
+
+Console.WriteLine($"{p1.Nome} {p1.Idade} {p1.Sexo}");
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "Manoel";
+p2.Idade = 23;
+p2.Sexo = "masculino";
+
+Console.WriteLine($"{p2.Nome} {p2.Idade} {p2.Sexo}");
+
+Console.ReadKey();
+
+class Pessoa
+{
+    public string? Nome { get; set; }
+    public int Idade { get; set; }
+    public string? Sexo { get; set; }
+}
