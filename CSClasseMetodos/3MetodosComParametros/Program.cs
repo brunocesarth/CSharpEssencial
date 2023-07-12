eec2efc96da1883dafcecb46969e3d91e7264bd2
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2023-07-12 20:18:39 -0300

    Aula - Métodos com parâmetros

---
+++ CSClasseMetodos/3MetodosComParametros/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("## Métodos com parametros ##\n");
+
+MinhaClasse minhaClasse = new MinhaClasse();
+
+var nomeCliente = "Maria";
+var dataAtual = DateTime.Now.ToShortDateString();
+
+// Valores dos argumentos
+minhaClasse.Saudacao(nomeCliente, dataAtual);
+
+Console.ReadKey();
+
+public class MinhaClasse
+{
+    // Os parametros do método
+    public void Saudacao(string nome, string data)
+    {
+        Console.WriteLine(nome);
+        Console.WriteLine(data);
+    }
+}
