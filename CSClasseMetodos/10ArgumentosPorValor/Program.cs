2a1bb0648718965c2049a68d58cd4209d337113b
Bruno Thobias <bruno.cm.thobias@hotmail.com>
2023-12-04 20:47:32 -0300

    Finalização aula - Passando argumentos por valor

--- CSClasseMetodos/10ArgumentosPorValor/Program.cs
+++ CSClasseMetodos/10ArgumentosPorValor/Program.cs
@@ -1,4 +1,20 @@
 ﻿Console.WriteLine("### Argumentos por valor ###\n");
 
+int x = 20;
+Console.WriteLine("\nValor do argumento x antes de passar por valor: " + x);
+
+Calculo calc = new();
+calc.Dobrar(x);
+
+Console.WriteLine("\nValor do argumento x depois de passar por valor: " + x);
 
 Console.ReadKey();
+
+public class Calculo
+{
+    public void Dobrar(int y)
+    {
+        y *= 2;
+        Console.WriteLine("\nValor do parâmetro y no método Dobrar " + y);
+    }
+}
