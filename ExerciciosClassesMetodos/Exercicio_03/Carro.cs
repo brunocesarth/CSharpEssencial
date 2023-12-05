3c83ada23104c03b20a77bf2b3d294bb7bd6c15f
Bruno Thobias <bruno.cm.thobias@hotmail.com>
2023-12-05 19:53:58 -0300

    Finalização exercicio 3

---
+++ ExerciciosClassesMetodos/Exercicio_03/Carro.cs
@@ -0,0 +1,47 @@
+﻿public class Carro
+{
+    public string? Modelo;
+    public string? Montadora;
+    public string? Marca;
+    public int Ano;
+    public int Potencia;
+
+
+    public Carro(string? Modelo, string? Montadora, string? Marca, int Ano, int Potencia)
+    {
+        this.Modelo = Modelo;
+        this.Montadora = Montadora;
+        this.Marca = Marca;
+        this.Ano = Ano;
+        this.Potencia = Potencia;
+    }
+
+    public Carro(string? modelo, string? montadora)
+    {
+        Modelo = modelo;
+        Montadora = montadora;
+    }
+
+    public void Acelerar(string marca)
+    {
+        Console.WriteLine("\nAcelerando o meu " + marca);
+    }
+
+    public double VelocidadeMaxima(int potencia)
+    {
+        return potencia * 1.75;
+    }
+
+    public int AumentarPotencia(int potencia)
+    {
+        potencia += 3;
+        return potencia;
+    }
+
+    public int AumentarPotencia(ref int potencia)
+    {
+        potencia += 5;
+        return potencia;
+    }
+
+}
---
+++ ExerciciosClassesMetodos/Exercicio_03/Program.cs
@@ -0,0 +1,30 @@
+﻿Carro chevrolet = new("Sedan", "Chevrolet", "Onix", 2016, 110);
+
+//chevrolet.Acelerar(chevrolet.Marca);
+
+Console.WriteLine($"{chevrolet.Modelo} {chevrolet.Montadora} {chevrolet.Marca}" +
+$" {chevrolet.Ano} {chevrolet.Potencia} CV");
+
+
+Console.WriteLine("Aumentando a Potencia (por valor : + 3CV) : ");
+var novaPotenciaChevrolet = chevrolet.AumentarPotencia(chevrolet.Potencia);
+Console.Write("-> " + novaPotenciaChevrolet);
+Console.WriteLine("\nO valor do campo Potencia não foi alterado : " + chevrolet.Potencia);
+
+
+Carro ford = new("SUV", "Ford", "EcoSport", 2018, 120);
+
+//ford.Acelerar(ford.Marca);
+
+Console.WriteLine($"{ford.Modelo} {ford.Montadora} {ford.Marca}" +
+$" {ford.Ano} {ford.Potencia} CV");
+
+Console.Write("\nAumentando a Potencia (por referência : +5CV) : ");
+var novaPotenciaFord = ford.AumentarPotencia(ref ford.Potencia);
+Console.Write("-> " + novaPotenciaFord);
+Console.WriteLine("\nO valor do campo Potencia foi alterado : " + ford.Potencia);
+
+
+
+Console.ReadKey();
+
