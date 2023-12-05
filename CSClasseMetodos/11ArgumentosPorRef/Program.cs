ff098ed2a3512e7887faa324a9f23b885d799440
Bruno Thobias <bruno.cm.thobias@hotmail.com>
2023-12-04 21:00:45 -0300

    Aula - passando argumentos por referência

---
+++ CSClasseMetodos/11ArgumentosPorRef/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("### Argumentos por referência ###\n");
+
+int x = 20;
+Console.WriteLine("\nValor do argumento x antes de passar por valor: " + x);
+
+Calculo calc = new();
+calc.Dobrar(ref x);
+
+Console.WriteLine("\nValor do argumento x depois de passar por valor: " + x);
+
+Console.ReadKey();
+
+
+public class Calculo
+{           
+    public void Dobrar(ref int y)
+    {
+        y *= 2;
+        Console.WriteLine("\nValor do parâmetro y no método Dobrar " + y);
+    }
+}
