8c5d564ade09f95a6cc85977ad7fe3a634ab42ce
Bruno Thobias <bruno.cm.thobias@hotmail.com>
2023-12-15 18:22:50 -0300

    Exercicio 5

---
+++ ExerciciosClassesMetodos/Exercicio_05/Program.cs
@@ -0,0 +1,10 @@
+﻿Carro ford = new("SUV", "Ford", "Ecosport", 2016, 110);
+
+ford.ExibirInfo(modelo: ford.Modelo, montadora: ford.Montadora, marca: ford.Marca, potencia: ford.Potencia);
+
+Console.WriteLine();
+
+ford.ExibirInfo(modelo: ford.Modelo, montadora: ford.Montadora, marca: ford.Marca, potencia: ford.Potencia, ano:  ford.Ano);
+
+
+Console.ReadKey();
