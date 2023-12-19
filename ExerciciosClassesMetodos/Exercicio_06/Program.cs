27027ae5ebf19ca6721a806d2cd9dd7e846512ef
Bruno Thobias <bruno.cm.thobias@hotmail.com>
2023-12-19 19:43:53 -0300

    Exercicio 6

---
+++ ExerciciosClassesMetodos/Exercicio_06/Program.cs
@@ -0,0 +1,12 @@
+﻿Carro.ObterValorIPVA();
+
+Console.WriteLine("Valor do ValorIpva: " + Carro.ValorIpva);
+
+
+Console.ReadKey();
+
+
+/*
+ * O modificador static permite declarar um membro estático que pertence ao próprio tipo e não a um objeto específico. Ele pode ser usado para declarar classes, structs e interfaces.
+ * Nas classes, structs e interfaces podemos usar o modificador static para campos, métodos, propriedades, operadores, eventos e construtores.
+ */
