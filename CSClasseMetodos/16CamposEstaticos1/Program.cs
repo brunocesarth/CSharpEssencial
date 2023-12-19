e580f15c7178f409e7c05bb47a3ff995bc3a2c56
Bruno Thobias <bruno.cm.thobias@hotmail.com>
2023-12-19 19:34:50 -0300

    Aula - Exercício - Campos estáticos

--- CSClasseMetodos/16CamposEstaticos1/Program.cs
+++ CSClasseMetodos/16CamposEstaticos1/Program.cs
@@ -1,6 +1,10 @@
-﻿Console.WriteLine("### Campos Estáticos 1");
+﻿Console.WriteLine("### Campos Estáticos 1 ###\n");
 
+string nome = Cliente.Nome = "João";
+int idade = Cliente.Idade = 15;
 
+Console.WriteLine($"Nome: {nome} \n" +
+    $"Idade: {idade} anos");
 
 Console.ReadKey();
 
@@ -18,4 +22,6 @@ public class Cliente
 
 /*
  * Um membro estático é definido usando o modificador static na declaração do membro
+ * Um campo estático não pertence ao objeto mas à classe
+ * Um campo(field) estático é compartilhado com todas as instâncias da classe e todas as instâncias acessam o mesmo local da memória
  */
---
+++ CSClasseMetodos/17ExPraticoCampoEstatico/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("### Campos estáticos - Exercício ### \n");
+
+ContaCorrente c1 = new();
+c1.Conta = 101;
+c1.Nome = "Maria";
+//c1.Juros = 3.99f;
+
+ContaCorrente c2 = new();
+c2.Conta = 102;
+c2.Nome = "Marta";
+//c2.Juros = 4.99f;
+
+ContaCorrente.Juros = 4.25f;
+
+Console.WriteLine($"Cliente: {c1.Nome} - Juros Anual: {c1.JurosAnual()}");
+Console.WriteLine($"Cliente: {c2.Nome} - Juros Anual: {c2.JurosAnual()}");
+
+Console.ReadKey();
+
+public class ContaCorrente
+{
+    public int Conta { get; set; }
+    public string? Nome { get; set; }
+    public static float Juros { get; set; }
+
+    public float JurosAnual()
+    {
+        return Juros * 12;
+    }
+}
