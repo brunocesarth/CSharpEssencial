8e927ae401c6d9b47b080427f0c8f2c03421ff58
Bruno Thobias <bruno.cm.thobias@hotmail.com>
2023-12-19 19:22:02 -0300

    Aula -  Campos estáticos I

---
+++ CSClasseMetodos/16CamposEstaticos1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("### Campos Estáticos 1");
+
+
+
+Console.ReadKey();
+
+public class Cliente
+{
+    public static string? Nome;
+    public static int Idade;
+
+    public Cliente(string? nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+    }
+}
+
+/*
+ * Um membro estático é definido usando o modificador static na declaração do membro
+ */
