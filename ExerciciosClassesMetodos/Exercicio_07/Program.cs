87df4ea64a06839c7ea30ec12fe9e805dd6f0f58
Bruno Thobias <bruno.cm.thobias@hotmail.com>
2023-12-20 19:29:00 -0300

    Exercicio 7

---
+++ ExerciciosClassesMetodos/Exercicio_07/Program.cs
@@ -0,0 +1,13 @@
+﻿Carro carro = new();
+carro.Ano = 1999;
+
+Console.WriteLine(carro.Ano);
+
+Console.ReadKey();
+
+/*
+ * As propriedades permitem que uma classe exponha uma forma pública de obter e definir valores, enquanto oculta o código de implementação ou de verificação.
+ * Propriedade pública Ano de leitura e gravação usando os acessadores get e set que expõe o campo privado ano.
+ * Um campo é uma variável de qualquer tipo que é declarada diretamente na classe.
+ * Uma propriedade é um membro que fornece um mecanismo flexível para ler, gravar ou calcular o valor de um campo privado.
+ */
