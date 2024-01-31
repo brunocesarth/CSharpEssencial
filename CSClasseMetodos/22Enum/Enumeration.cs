7d48e7f9d5ffcd009672105820169c95027f96c1
Bruno Thobias <90666740+brunocesarth@users.noreply.github.com>
2024-01-31 18:52:10 -0300

    Aula - Enum

---
+++ CSClasseMetodos/22Enum/Enumeration.cs
@@ -0,0 +1,22 @@
+﻿namespace _22Enum;
+
+enum DiasDaSemana
+{
+    Segunda,    //0
+    Terça,      //1
+    Quarta,     //2
+    Quinta,     //3
+    Sexta,      //4
+    Sábado,     //5
+    Domingo     //6
+}
+
+enum Categorias
+{
+    Moda,
+    Automotivo,
+    Artes,
+    Bebidas,
+    Livros,
+    Brinquedos
+}
---
+++ CSClasseMetodos/22Enum/Program.cs
@@ -0,0 +1,28 @@
+﻿using _22Enum;
+
+Console.WriteLine("### ENUM ###\n");
+
+Console.WriteLine($"{Categorias.Moda} - {(int)Categorias.Moda}");
+Console.WriteLine($"{Categorias.Automotivo} - {(int)Categorias.Automotivo}");
+Console.WriteLine($"{Categorias.Artes} - {(int)Categorias.Artes}");
+Console.WriteLine($"{Categorias.Bebidas} - {(int)Categorias.Bebidas}");
+Console.WriteLine($"{Categorias.Livros} - {(int)Categorias.Livros}");
+Console.WriteLine($"{Categorias.Brinquedos} - {(int)Categorias.Brinquedos}");
+
+Console.WriteLine("\nSelecione a Categoria teclando o seu valor");
+int valor = Convert.ToInt32(Console.ReadLine());
+
+var nomeMembroEnum = (Categorias)valor;
+Console.WriteLine($"\nVocê selecionou a categoria : {nomeMembroEnum.ToString()}");
+
+Console.WriteLine(DiasDaSemana.Domingo);
+Console.WriteLine(DiasDaSemana.Terça);
+
+int dia1 = (int)DiasDaSemana.Domingo;
+int dia2 = (int)DiasDaSemana.Terça;
+
+
+Console.WriteLine($"\n{DiasDaSemana.Domingo} vale: {dia1}");
+Console.WriteLine($"\n{DiasDaSemana.Terça} vale: {dia2}");
+
+Console.ReadKey();
