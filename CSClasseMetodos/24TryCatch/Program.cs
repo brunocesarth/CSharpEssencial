7f24f97286dbd9ee12b4c111913f462aa039fd07
Bruno Thobias <90666740+brunocesarth@users.noreply.github.com>
2024-02-08 18:07:34 -0300

    Aula Try-Catch

---
+++ CSClasseMetodos/24TryCatch/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("### BLOCO TRY-CATCH ###\n");
+
+Console.WriteLine("## Divisão por números inteiros ##");
+Console.WriteLine("x/y");
+Console.WriteLine("\nInforme o valor de x ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nInforme o valor de y ");
+int y = Convert.ToInt32(Console.ReadLine());
+try
+{
+    int z = x / y;
+    Console.WriteLine($"\n{x} / {y} = {z}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("\nNão existe divisão por zero, tente outro número...");
+    Console.WriteLine($"\n Erro: <<< {ex.Message} >>>");
+    Console.WriteLine($"\n Detalhes {ex?.StackTrace?.ToString()}");
+}
+finally
+{
+    Console.WriteLine("Processamento concluído...");
+}
+
+
+Console.ReadKey();
