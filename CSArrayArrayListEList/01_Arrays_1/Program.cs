9dd61262da5e4daf75dcc0b61c3f34420891b428
Bruno Thobias <90666740+brunocesarth@users.noreply.github.com>
2024-04-10 21:04:23 -0300

    Aula - Arrays 1

---
+++ CSArrayArrayListEList/01_Arrays_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Declaração do array
+int[] numeros;
+
+// Aloquei Memoria
+numeros = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+string[] nomes = new string[5] { "Ana", "Maria", "Marta", "Paulo", "Carlos" };
+
+Console.WriteLine(nomes[0]);
+Console.WriteLine(nomes[1]);
+Console.WriteLine(nomes[2]);
+Console.WriteLine(nomes[3]);
+Console.WriteLine(nomes[4]);
+
+//Declarando um array
+int[] numeros1;
+
+//alocar memoria
+numeros1 = new int[3];
+numeros1[0] = 1;
+numeros1[1] = 2;
+numeros1[2] = 3;
+
+
+Console.ReadKey();
