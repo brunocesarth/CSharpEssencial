d813d74651b88ba2b5d5664f47e44e9852905281
Bruno Thobias <90666740+brunocesarth@users.noreply.github.com>
2024-04-17 20:44:21 -0300

    Aula - Arrays III

---
+++ CSArrayArrayListEList/03_Arrays_3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine(" ### Arrays 3 ### \n");
+
+int[] numeros;
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+string[] nomes = { "Ana", "Maria", "Marta", "Paulo", "Carlos" };
+
+foreach (int numero in numeros)
+{
+    Console.Write($"{numero} ");
+}
+Console.WriteLine();
+
+foreach (string nome in nomes)
+{
+    Console.Write($"{nome} ");
+}
+
+Console.ReadKey();
