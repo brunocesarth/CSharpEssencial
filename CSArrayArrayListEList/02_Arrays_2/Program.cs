1c7b3a2575b651f32edf27b50082b626f28160bb
Bruno Thobias <90666740+brunocesarth@users.noreply.github.com>
2024-04-17 19:21:28 -0300

    Aula - Arrays II

---
+++ CSArrayArrayListEList/02_Arrays_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine(" ### Arrays II ### \n");
+
+int[] numeros;
+
+numeros = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+string[] nomes = { "Ana", "Maria", "Marta", "Paulo", "Carlos" };
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"Elemento de índice {i}: {numeros[i]}");
+}
+
+
+for (int i = 0; i < nomes.Length; i++)
+{
+    Console.WriteLine($"Elemento de índice {i}: {nomes[i]}");
+}
+
+Console.ReadKey();
