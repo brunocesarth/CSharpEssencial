ec8d0d6c7d27c0185582cf2390815447eb804fa1
Bruno Thobias <90666740+brunocesarth@users.noreply.github.com>
2024-05-21 16:45:07 -0300

    Aula - Exercício - Arrays Bidimensionais

---
+++ CSArrayArrayListEList/09_ArrayBidimensional-Exercicio/Program.cs
@@ -0,0 +1,20 @@
+﻿string[,] alunos = new string[2, 5];
+
+for (int i = 0; i < alunos.GetLength(0); i++)
+{
+    for (int j = 0; j < alunos.GetLength(1); j++)
+    {
+        Console.Write($"Digite o aluno para a posição [{i},{j}]: ");
+        alunos[i, j] = Console.ReadLine();
+    }
+}
+
+for (int i = 0; i < alunos.GetLength(0); i++)
+{
+    for (int j = 0; j < alunos.GetLength(1); j++)
+    {
+        Console.WriteLine($"Aluno: {alunos[i, j]} no indice [{i},{j}]");
+    }
+}
+
+Console.ReadKey();
