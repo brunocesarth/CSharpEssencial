c91fbb512f5a2ecfdf1d0ddb21f244ff7e513020
Bruno Thobias <90666740+brunocesarth@users.noreply.github.com>
2024-06-19 20:37:12 -0300

    Aula - Serialização XML

---
+++ CSSerializacaoDesserializacao/03_SerializacaoXML/Aluno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_SerializacaoXML
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public int Idade;
+
+        public Aluno()
+        {
+
+        }
+
+        public Aluno(int id, string name, string email, int idade)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Idade = idade;
+        }
+    }
+}
