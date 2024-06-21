6df5bb18dbef82f0117cd5207c8e1be3fd35d9d6
Bruno Thobias <90666740+brunocesarth@users.noreply.github.com>
2024-06-20 21:33:14 -0300

    Aula - Desserialização JSON

---
+++ CSSerializacaoDesserializacao/06_DesserializaçãoJSON/Aluno.cs
@@ -0,0 +1,20 @@
+﻿namespace _06_DesserializaçãoJSON
+{
+    internal class Aluno
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Idade { get; set; }
+
+        public Aluno() { }
+
+        public Aluno(int id, string name, string email, int idade)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Idade = idade;
+        }
+    }
+}
---
+++ CSSerializacaoDesserializacao/06_DesserializaçãoJSON/Program.cs
@@ -0,0 +1,13 @@
+﻿using _06_DesserializaçãoJSON;
+using System.Text.Json;
+
+var caminhoArquivo = @"d:\dados\Serializados\AlunoSerializado.json";
+
+string jsonContent = File.ReadAllText(caminhoArquivo);
+
+var aluno = JsonSerializer.Deserialize<Aluno>(jsonContent);
+
+Console.WriteLine($"Aluno desserializado - ID: {aluno.Id}, Nome: {aluno.Name}, " +
+                  $"Email: {aluno.Email}, Idade: {aluno.Idade}");
+
+Console.ReadKey();
