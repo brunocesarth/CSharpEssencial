c7fe5320d5ae0d552516e0319c00c2156200fb1d
Bruno Thobias <90666740+brunocesarth@users.noreply.github.com>
2024-06-22 17:43:58 -0300

    Aula - ArrayList III

---
+++ CSArrayArrayListEList/12_ArrayList_3/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+
+var lista = new ArrayList() { "Ana", 5, true, " ", 1.1, "Zé", 3, 9, null };
+
+lista.Remove(null);
+
+lista.RemoveAt(4);
+
+lista.RemoveRange(0, 2);
+
+Console.ReadKey();
