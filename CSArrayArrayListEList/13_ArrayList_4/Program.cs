6663f8c3348e87d0d65a2a926852910617b7a9f5
Bruno Thobias <90666740+brunocesarth@users.noreply.github.com>
2024-06-22 18:10:42 -0300

    Aula - ArrayList IV

---
+++ CSArrayArrayListEList/13_ArrayList_4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+var lista = new ArrayList() { "Maria", 5, true, 4.5, null, DateTime.Now };
+
+//Verificar se um elemento existe na coleção
+
+var res1 = lista.Contains(5);           //true
+bool res2 = lista.Contains("Mari");     //false
+
+Console.WriteLine(res1);
+Console.WriteLine(res2);
+Console.WriteLine(lista.Contains(null));
+
+var list = new ArrayList() { "Maria", "Ana", "Zilda", "Carlos", "Diná" };
+
+Console.WriteLine("\nArrayList original \n");
+foreach (var item in list)
+{
+    Console.WriteLine($"{item}");
+}
+
+list.Sort();
+
+Console.WriteLine("\nNúmero de elementos no array " + list.Count);
+Console.WriteLine("\nArrayList ordenado \n");
+foreach (var item in list)
+{
+    Console.WriteLine($"{item}");
+}
+
+list.Clear();
+Console.WriteLine("\nNúmero de elementos no array depois do Clear " + list.Count);
+
+Console.ReadKey();
