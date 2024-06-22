7de16843b4bfa0e4502de1794e16f1708d05d6a0
Bruno Thobias <90666740+brunocesarth@users.noreply.github.com>
2024-06-21 22:13:01 -0300

    Aula - ArrayList II

---
+++ CSArrayArrayListEList/11_ArrayList_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+
+var lista = new ArrayList() { "Maria", 5, true, " ", null };
+//  0      1   2     3     4
+
+lista.Add(3.5);
+
+lista.Insert(2, "Paulo");
+
+int[] array1 = { 1, 2, 3 };
+lista.AddRange(array1);
+
+lista.InsertRange(2, array1);
+
+Console.ReadKey();
+
+
