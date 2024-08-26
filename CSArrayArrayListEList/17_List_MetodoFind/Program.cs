97533e9ece7ac2238e6205537e77ffc2c4e42ec3
Bruno Thobias <90666740+brunocesarth@users.noreply.github.com>
2024-08-26 14:03:40 -0300

    Aula - List: Métodos Find

---
+++ CSArrayArrayListEList/17_List_MetodoFind/Program.cs
@@ -0,0 +1,22 @@
+﻿
+List<string> frutas = new() {
+    "Uva","Banana", "Pera","Maça","Abacate","Laranja", "Morango"
+};
+
+// Usando predicado como uma função
+var fruta1 = frutas.Find(Procura);
+Console.WriteLine($"\nPredicado => {fruta1} ");
+
+// Usando a expressão lambda
+var fruta2 = frutas.Find(i => i.Contains('n'));
+Console.WriteLine($"\nExpressão lambda => {fruta2} ");
+
+
+Console.ReadKey();
+
+
+
+static bool Procura (string item)
+{
+    return item.StartsWith('L');
+}
