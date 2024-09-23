7e2392cafd32af8e87d4dfcbda0d82f118334698
Bruno Thobias <90666740+brunocesarth@users.noreply.github.com>
2024-09-23 15:14:09 -0300

    Aula - Herança - Construtores

---
+++ CSPOO/04_HerancasContrutores/Program.cs
@@ -0,0 +1,34 @@
+﻿Aluno aluno1 = new();
+Aluno aluno2 = new("José");
+
+Console.ReadKey();
+
+//Base
+class Pessoa
+{
+    public Pessoa()
+    {
+        Console.WriteLine("Construtor da classe Pessoa");
+    }
+
+    public Pessoa(string nome)
+    {
+        Console.WriteLine("Construtor da classe Pessoa com parametro");
+    }
+}
+
+//Derivada  
+class Aluno : Pessoa
+{
+    public Aluno() : base()
+    {
+        Console.WriteLine("Construtor da classe Aluno");
+    }
+
+    public Aluno(string nome) : base(nome)
+    {
+        {
+            Console.WriteLine("Construtor da classe Aluno com parametro");
+        }
+    }
+}
