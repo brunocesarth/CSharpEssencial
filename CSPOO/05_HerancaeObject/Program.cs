1ba5daf22613c578cf488846819fdf9a0685a065
Bruno Thobias <90666740+brunocesarth@users.noreply.github.com>
2024-09-24 06:28:39 -0300

    Aula - Herança e Object

---
+++ CSPOO/05_HerancaeObject/Program.cs
@@ -0,0 +1,13 @@
+﻿Nada nada = new();
+
+nada.Equals(nada);
+
+Console.ReadKey();
+
+public class Nada { }
+
+public class Classe1 { }
+
+public class Classe2 { }
+
+public class ClasseDerivada : Classe1 { }
