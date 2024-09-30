c440b2d58245da349a2908597598878ca34c7557
Bruno Thobias <90666740+brunocesarth@users.noreply.github.com>
2024-09-30 12:54:42 -0300

    Aula - Herança - virtual e override

---
+++ CSPOO/07_HerancaVirtualeOverride/Program.cs
@@ -0,0 +1,28 @@
+﻿var gato = new Gato() { Nome = "Bichano" };
+gato.ExibeNome();
+
+var cao = new Cachorro() { Nome = "Pipoca" };
+cao.ExibeNome();
+
+Console.ReadKey();
+
+// Classe base
+class Animal
+{
+    public string? Nome { get; set; }
+    public virtual void ExibeNome()
+    {
+        Console.WriteLine($"\nMeu nome é {Nome}.");
+    }
+}
+
+// Classe derivada
+class Gato : Animal
+{
+    public override void ExibeNome()
+    {
+        Console.WriteLine($"\nEu sou um gato. Meu nome é: {Nome}");
+    }
+}
+
+class Cachorro : Animal { }
